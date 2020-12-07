35035bfa164ea6a46f9e941a2265719be169b311
Vic Wu <madcocomo@gmail.com>
2020-12-07 23:25:17 +0800

    apply question2 check and test

--- src/test/java/com/gildedrose/atvwgr/AppRunnerTest.java
+++ src/test/java/com/gildedrose/atvwgr/AppRunnerTest.java
@@ -1,5 +1,6 @@
-package com.gildedrose;
+package com.gildedrose.atvwgr;
 
+import com.gildedrose.AppRunner;
 import com.google.common.base.Charsets;
 import org.junit.Test;
 
@@ -9,7 +10,6 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
@@ -19,8 +19,8 @@ public class AppRunnerTest {
     public void safeNetTest() throws URISyntaxException, IOException {
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        AppRunner.main(new String[]{getClass().getResource("/items.txt").getPath(), "6"});
-        byte[] bytes = Files.readAllBytes(Paths.get(getClass().getResource("/expected.txt").toURI()));
+        AppRunner.main(new String[]{getClass().getResource("/atvwgr/items.txt").getPath(), "6"});
+        byte[] bytes = Files.readAllBytes(Paths.get(getClass().getResource("/atvwgr/expected.txt").toURI()));
         String expected = new String(bytes, Charsets.UTF_8);
         assertEquals(expected, out.toString());
     }
